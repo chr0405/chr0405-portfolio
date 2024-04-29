4d8d5d89cd526a34c521dad7575586317685e547
chr0405 <ch0nn@naver.com>
2024-04-29 23:34:45 +0900

    intro 마우스 추가

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 import styles from "./componentsStyles/Intro.module.css";
+import Image from "next/image";
+import mouse from "../img/mouse.png";
 
 export default function Intro() {
 
@@ -29,11 +31,13 @@ export default function Intro() {
             const x = getRandomValue(windowWidth);
             const y = getRandomValue(windowHeight);
 
+            const className = `${styles[style[styleOption]]} ${styles[opacity[opacityOption]]} ${styles[twinkle[twinkleOption]]}`;
             const styleName = `${styles[style[styleOption]]}`;
             const opacityName = `${styles[opacity[opacityOption]]}`;
             const twinkleName = `${styles[twinkle[twinkleOption]]}`;
 
             starsArray.push({
+                className: className,
                 styleName: styleName,
                 opacityName: opacityName,
                 twinkleName: twinkleName,
@@ -54,11 +58,18 @@ export default function Intro() {
             <div className={styles.totalDiv}>
                 <div className={styles.textDiv}>
                     <h1 className={click? '' : styles.h1TwinklingSetting}
-                        // onClick={() => setClick(prev => !prev)}
-                        onMouseEnter={() => setClick(prev => !prev)}
-                        onMouseLeave={() => setClick(prev => !prev)}>
-                        타이틀입니다. 타이틀입니다. 타이틀입니다. 타이틀입니다.
+                        onClick={() => setClick(prev => !prev)}
+                        // onMouseEnter={() => setClick(true)}
+                        // onMouseLeave={() => setClick(prev => !prev)}
+                        >
+                        안녕하세요.<br/>Front-end 개발자 조하림입니다.
                     </h1>
+                    <div style={{ display : click? 'none' : 'flex' }}>
+                        <Image src={mouse} alt="intro mouse"
+                            className={click? '' : styles.imgMovingSetting}
+                            onClick={() => setClick(prev => !prev)}
+                        />
+                    </div>
                     <p>
                         내용입니다. 내용입니다. 내용입니다. 내용입니다.<br/>
                         내용입니다. 내용입니다. 내용입니다. 내용입니다.<br/>
@@ -68,10 +79,10 @@ export default function Intro() {
             </div>
             {click && stars.map((star, index) => (
                 <div key={index}>
-                    <div className={`${styles.star} ${star.styleName} ${star.opacityName} ${star.twinkleName}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
-                    <div className={`${styles.star2} ${star.styleName} ${star.opacityName} ${star.twinkleName}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
-                    <div className={`${styles.star3} ${star.styleName} ${star.opacityName} ${star.twinkleName}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
-                    <div className={`${styles.star4} ${star.styleName} ${star.opacityName} ${star.twinkleName}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
+                    <div className={`${styles.star} ${star.className}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
+                    <div className={`${styles.star2} ${star.className}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
+                    <div className={`${styles.star3} ${star.className}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
+                    <div className={`${styles.star4} ${star.className}`} style={{ position: 'absolute', top: star.y, left: star.x }}></div>
                 </div>
             ))}
         </>
