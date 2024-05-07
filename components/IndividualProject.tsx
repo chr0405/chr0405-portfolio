a9aef2db68b762be77b7247a811025ee85be8261
chr0405 <ch0nn@naver.com>
2024-05-07 22:12:38 +0900

    IndividualProject 추가 & 연결

---
+++ components/IndividualProject.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./componentsStyles/IndividualProject.module.css"
+
+import DemoImg from "../img/home.png";
+import YouTubeImg from "../img/youTube.png";
+import GitHub from "../img/gitHub.png";
+import { useState } from "react";
+
+interface IndividualProjectProps {
+    capture : any[];
+    name : string;
+    period : string;
+    aboutProject : string;
+    webSite : {
+        Demo?: string;
+        YouTube?: string;
+        GitHub?: string;
+    };
+}
+
+const IndividualProject : React.FC<IndividualProjectProps> = ({ capture, name, period, aboutProject, webSite }) => {
+    
+    const [captureNum, setCaptureNum] = useState(0);
+    
+    return (
+        <div className={styles.totalDiv}>
+            {/* 프로젝트 */}
+            <div className={styles.projectDiv}>
+                <div className={styles.projectNameDiv}>{name}</div>
+
+                {/* 사진 */}
+                <div className={styles.projectPictureDiv}>
+                    <Image src={capture[captureNum]} alt="captureImg"/>
+                    <div className={styles.projectPictureSelectTotalDiv}
+                        style={{display : capture.length > 1 ? 'flex' : 'none'}}>
+                        {[...Array(capture.length)].map((_, index) => (
+                            <div className={styles.projectPictureSelectDiv}
+                                style={{backgroundColor : index === captureNum ? '#4D4D4D' : '#C6C6C6'}}
+                                key={index}
+                                onClick={() => {
+                                    setCaptureNum(index);
+                                }}>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.namePeriodDiv}>
+                        <h1>{name}</h1>
+                        <p>{period}</p>
+                    </div>
+                    <h6 className={styles.aboutProject}>{aboutProject}</h6>
+                    <p className={styles.more}>More →</p>
+                </div>
+            </div>
+
+            {/* 바로가기 */}
+            <div className={styles.webSiteDiv}>
+                {webSite && webSite.Demo && (
+                    <a className={styles.webSite}
+                    href={webSite.Demo}>
+                        <Image src={DemoImg} alt="DemoImg"/>
+                        Demo
+                    </a>
+                )}
+                {webSite && webSite.YouTube && (
+                    <a className={styles.webSite} href={webSite.YouTube}>
+                        <Image style={{
+                            "width" : "2.78vw",
+                            "height" : "2.78vw",
+                            "marginBottom" : "0.05vw",
+                        }}
+                            src={YouTubeImg} alt="YouTubeImg"/>
+                        YouTube
+                    </a>
+                )}
+                {webSite && webSite.GitHub && (
+                    <a className={styles.webSite}
+                    href={webSite.GitHub}>
+                        <Image src={GitHub} alt="GitHub"/>
+                        GitHub
+                    </a>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default IndividualProject;
--- components/Project.tsx
+++ components/Project.tsx
@@ -1,8 +1,22 @@
+import IndividualProject from "./IndividualProject"
 import styles from "./componentsStyles/Project.module.css"
+import { ProjectArray } from "../array/ProjectArray"
 
 export default function Project() {
     return (
         <div className={styles.totalDiv}>
+            <div className={styles.projectBoxDiv}>
+                { ProjectArray.map((project, index) => (
+                    <IndividualProject
+                        key={index}
+                        capture = {project.capture}
+                        name = {project.name}
+                        period = {project.period}
+                        aboutProject = {project.aboutProject}
+                        webSite = {project.webSite}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
