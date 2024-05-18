88123bcdd80cac3491335b0d5ced84598f6fb5cc
chr0405 <ch0nn@naver.com>
2024-05-18 13:56:53 +0900

    모달 on 시에 주소 변경

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,34 +1,15 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-import LoginModal from '../../components/LoginModal';
+import Intro from "../../components/Intro";
+import Profile from "../../components/Profile";
+import Project from "../../components/Project";
+import Skill from "../../components/Skill";
 
 export default function Home() {
-    const pathname = usePathname();
-    const [modalShow, setModalShow] = useState(false);
-
-    useEffect(() => {
-        if (pathname === '/log-in') {
-            setModalShow(true);
-        }
-    }, [pathname]);
-
-    const openModal = () => {
-        setModalShow(true);
-        window.history.pushState(null, '', '/log-in');
-    };
-
-    const closeModal = () => {
-        setModalShow(false);
-        window.history.pushState(null, '', '/');
-    };
-
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <button onClick={openModal}>Login</button>
-            <LoginModal show={modalShow} onClose={closeModal} />
-        </div>
-    );
+        <>
+            <div id="intro"><Intro/></div>
+            <div id="profile"><Profile/></div>
+            <div id="skill"><Skill/></div>
+            <div id="project"><Project/></div>
+        </>
+    )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   )
